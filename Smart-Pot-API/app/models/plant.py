43f93ed4e3a551faaeb1d3f4c86792c9c655cc65
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-04-19 21:05:53 +0200

    Add communication between tables in database

--- Smart-Pot-API/app/models/plant.py
+++ Smart-Pot-API/app/models/plant.py
@@ -1,16 +1,16 @@
 from app.db.base import Base
-from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy import Column, Float, Integer, String,ForeignKey
 from sqlalchemy.orm import relationship
-from typing import TYPE_CHECKING
+
 """Database plant model"""
 
-if TYPE_CHECKING:
-    from .user import User  # noqa: F401
 
 class Plant(Base):
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True, nullable=False)
     humidity = Column(Float, nullable=True)  # nullable set to True it means that we can have nulls
     lux = Column(Float, nullable=True)
     temperature = Column(Float, nullable=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
     owner_id = relationship("User", back_populates="plants")
--- Smart-Pot-API/app/models/user.py
+++ Smart-Pot-API/app/models/user.py
@@ -1,15 +1,12 @@
 from app.db.base import Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Boolean, Column, Integer, String
-from typing import TYPE_CHECKING
 
 """ Database user model"""
 
-if TYPE_CHECKING:
-    from .plant import Plant  # noqa: F401
-
 
 class User(Base):
+
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, index=True, nullable=False)
     email = Column(String, index=True, unique=True, nullable=False)
