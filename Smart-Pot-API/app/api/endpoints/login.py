4514aef6628d38ca283e91e976066e71bec76099
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-04-21 13:44:44 +0200

    Add endpoint for login user and retype acces-token

--- Smart-Pot-API/app/api/endpoints/login.py
+++ Smart-Pot-API/app/api/endpoints/login.py
@@ -6,8 +6,8 @@ from app.schemas.message import Message
 from app.schemas.user import UserCreate
 from sqlalchemy.orm import Session
 from app.core.dependencies import get_db
-from app.crud.crud_users import user_authentication, user_is_active, create_new_user, get_user_by_email
-from typing import Any
+from app.crud.crud_users import user_authentication, create_new_user, get_user_by_email
+from typing import Any, Annotated
 from app.core.settings import settings
 from datetime import timedelta
 from app.core.security import create_access_token
@@ -15,14 +15,13 @@ from app.core.security import create_access_token
 router = APIRouter(prefix="/api/v1", tags=[Tag.LOGIN])
 
 
-@router.post("/login/access-token", response_model=Token)
-def login_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
+@router.post("/token", response_model=Token)
+def login_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                       db: Session = Depends(get_db)) -> Any:
     user = user_authentication(db=db, user_email=form_data.username, password=form_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Cannot find user")
-
-    elif not user_is_active(user=user):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password",
+                            headers={"WWW-Authenticate": "Bearer"})
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
     return {"access_token": create_access_token(subject=user.email, expires_delta=access_token_expires),
---
+++ Smart-Pot-API/app/api/endpoints/users.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+from app.api.endpoints.tags import Tag
+from app.schemas.user import User
+from typing import Annotated
+from app.crud.crud_users import get_current_active_user
+
+router = APIRouter(prefix="/api/v1/users", tags=[Tag.LOGIN])
+
+
+@router.get("/me", response_model=User)
+def get_current_user(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
