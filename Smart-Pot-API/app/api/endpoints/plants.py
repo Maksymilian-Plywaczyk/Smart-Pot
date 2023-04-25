400dabef65742de7b19804e8a8efc76043d5bcd7
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-04-25 22:36:57 +0200

    Create endpoint for read plant by id

--- Smart-Pot-API/app/api/endpoints/plants.py
+++ Smart-Pot-API/app/api/endpoints/plants.py
@@ -1,13 +1,26 @@
 from typing import Annotated, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
 
 from app.api.endpoints.tags import Tag
-from app.crud.crud_plants import get_current_user_plants
+from app.core.dependencies import get_db
+from app.crud.crud_plants import get_current_user_plants, get_plant_by_id
+from app.schemas.plant import Plant
 
 router = APIRouter(prefix="/api/v1/plants", tags=[Tag.PLANTS])
 
 
-@router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK, response_model=List[Plant])
 def get_current_user_plants(plants: Annotated[List, Depends(get_current_user_plants)]):
     return plants
+
+
+@router.get("/{plant_id}", status_code=status.HTTP_200_OK, response_model=Plant)
+def read_plant_by_id(plant_id: int, db: Session = Depends(get_db)) -> Plant:
+    plant = get_plant_by_id(plant_id=plant_id, db=db)
+    if plant is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Cannot find plant"
+        )
+    return plant
--- Smart-Pot-API/app/crud/crud_plants.py
+++ Smart-Pot-API/app/crud/crud_plants.py
@@ -9,7 +9,7 @@ from app.models.user import User
 
 
 def get_plant_by_id(db: Session, plant_id: int):
-    plant_by_id = db.query(Plant).filter(Plant.plant_id == plant_id).first()
+    plant_by_id = db.query(Plant).filter(Plant.id == plant_id).first()
     return plant_by_id
 
 
