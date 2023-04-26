609f42aabb03e5f49ca7386043f5b477b5bca2f2
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-04-26 12:01:47 +0200

    Delete Base declaration

--- Smart-Pot-API/app/db/session.py
+++ Smart-Pot-API/app/db/session.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from app.core.settings import settings
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
