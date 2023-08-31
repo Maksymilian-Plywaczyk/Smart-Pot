d777a2671e487c97de42addcea3a3b7b0ec18a61
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-08-31 18:17:25 +0200

    Create schemas necessary for device serialization and validation

---
+++ Smart-Pot-API/app/schemas/device.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field
+
+from app.schemas.utils.device_enums import DeviceNames
+
+
+class DeviceBase(BaseModel):
+    name: str = Field(..., description="Device name")
+    type: str = Field(..., description="Device type")
+
+
+class DeviceResponse(BaseModel):
+    id: str
+    name: str
+    type: str
+    user_id: int
+
+
+class DeviceCreate(DeviceBase):
+    type: DeviceNames = Field(..., description="Device type")
+
+
+class DeviceUpdate(DeviceBase):
+    pass
+
+
+class DeviceDB(DeviceBase):
+    id: str
+    device_token: str
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Device(DeviceDB):
+    pass
