326b8b49b172c4e77bf8142b1916a369f641f804
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-08-31 18:19:40 +0200

    Create CRUD methods and include devices endpoint to api

---
+++ Smart-Pot-API/app/api/endpoints/devices.py
@@ -0,0 +1,73 @@
+from typing import Annotated, Any, List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.api.endpoints.tags import Tag
+from app.core.dependencies import get_db
+from app.core.security import create_device_token
+from app.crud.crud_devices import (
+    create_new_device,
+    delete_user_device,
+    get_current_user_devices,
+    get_device_by_id,
+)
+from app.crud.crud_users import get_current_active_user, get_current_user
+from app.models.user import User
+from app.schemas.device import Device, DeviceCreate, DeviceResponse
+from app.schemas.message import Message
+
+router = APIRouter(prefix="/api/v1/devices", tags=[Tag.DEVICES])
+
+
+@router.get("/", status_code=200, response_model=List[Device])
+def get_current_user_devices(
+    devices: Annotated[List[Device], Depends(get_current_user_devices)]
+):
+    return devices
+
+
+@router.get("/{device_id}", status_code=200, response_model=Device)
+def read_device_by_id(device_id: str, db=Depends(get_db)) -> Device:
+    device = get_device_by_id(db=db, device_id=device_id)
+    if device is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Cannot find device"
+        )
+    return device
+
+
+@router.post(
+    "/create-new-device",
+    status_code=status.HTTP_201_CREATED,
+    response_model=DeviceResponse,
+)
+def create_new_device_for_current_user(
+    new_device: DeviceCreate,
+    current_user: Annotated[User, Depends(get_current_user)],
+    db=Depends(get_db),
+):
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    device_token = create_device_token(length_token=15)
+    new_device = create_new_device(
+        new_device, db, current_user.id, device_token=device_token
+    )
+    return DeviceResponse(
+        id=new_device.id,
+        name=new_device.name,
+        type=new_device.type,
+        user_id=new_device.user_id,
+    )
+
+
+@router.delete("/{device_id}", status_code=status.HTTP_200_OK, response_model=Message)
+def delete_device_user(
+    device_id: str,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    db=Depends(get_db),
+) -> Any:
+    user_email = current_user.email
+    deleted_message = delete_user_device(db, device_id, user_email)
+    return deleted_message
--- Smart-Pot-API/app/main.py
+++ Smart-Pot-API/app/main.py
@@ -2,8 +2,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import app.models.device as device_model
 import app.models.plant as plant_model
 import app.models.user as user_model
+from app.api.endpoints.devices import router as devices_router
 from app.api.endpoints.login import router as login_router
 from app.api.endpoints.plants import router as plants_router
 from app.api.endpoints.users import router as users_router
@@ -14,7 +16,7 @@ app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
 user_model.Base.metadata.create_all(bind=engine)
 plant_model.Base.metadata.create_all(bind=engine)
-
+device_model.Base.metadata.create_all(bind=engine)
 origins = [
     "http://localhost",
     "http://localhost:8080",
@@ -33,3 +35,4 @@ app.add_middleware(
 app.include_router(login_router)
 app.include_router(users_router)
 app.include_router(plants_router)
+app.include_router(devices_router)
