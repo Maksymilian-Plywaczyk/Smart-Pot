445faab40123fe42d9620b7d67263ba9300639ea
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-08-31 18:22:38 +0200

    Create function which create authentication token for devices

--- Smart-Pot-API/app/core/security.py
+++ Smart-Pot-API/app/core/security.py
@@ -1,3 +1,5 @@
+import secrets
+import string
 from datetime import datetime, timedelta
 from typing import Optional, Union
 
@@ -25,6 +27,18 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_context.verify(plain_password, hashed_password)
 
 
+def verify_nodemcu_token(nodemcu_token: str) -> bool:
+    if nodemcu_token == settings.NODEMCU_KEY:
+        return True
+    return False
+
+
+def verify_device_name_with_updated_name(device_in: str, device_updated) -> bool:
+    if device_in == device_updated:
+        return True
+    return False
+
+
 def create_access_token(
     subject: Union[str, any], expires_delta: Optional[timedelta] = None
 ) -> str:
@@ -38,6 +52,11 @@ def create_access_token(
     to_encode = {"exp": expires_delta, "subject": str(subject)}
     encoded_jwt = jwt.encode(claims=to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def create_device_token(length_token: int) -> str:
+    characters = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(characters) for _ in range(length_token))
 
 
 def verify_access_token(token: str) -> Optional[str]:
