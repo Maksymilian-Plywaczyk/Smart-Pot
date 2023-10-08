7ded78c87f56caf69a7728a866126634b99ae1c5
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-10-08 15:30:51 +0200

    create validation which, check if provided language is in Languages values

--- Smart-Pot-API/app/schemas/utils/languages.py
+++ Smart-Pot-API/app/schemas/utils/languages.py
@@ -1,6 +1,19 @@
 from enum import Enum
 
+from pydantic import BaseModel, validator
+
 
 class Languages(str, Enum):
     POLISH: str = "PL"
     ENGLISH: str = "ENG"
+
+
+class UpdateUserLanguage(BaseModel):
+    language: Languages
+
+    @validator("language")
+    def validate_language(cls, value):
+        language_options = [language.value for language in Languages]
+        if value not in language_options:
+            raise ValueError("Invalid language provided")
+        return value
